e1a9c135ae3d34068e29f161b89123bffabec030
Ryan Roth <ryanroth360@gmail.com>
2024-06-21 11:37:09 -0700

    waits for crdownload to finish

--- main.py
+++ main.py
@@ -1,28 +1,21 @@
 import web
-import time
-
+import argparse
 
 def read_file_lines(file_path):
-    
+    driver = web.create_driver()
     try:
         with open(file_path, 'r') as file:
             lines = file.readlines()
         for line in lines:
-            driver = web.create_driver()
             line = line.strip()
             web.download(driver, line)
     except FileNotFoundError:
         print(f"The file at {file_path} was not found.")
         return []
-    time.sleep(5)
-
-# Example usage
-
-
-def main():
-    pass
-
 
 if __name__ == "__main__":
-    file_path = '/Users/ryanroth/Desktop/DJ Converter/djConverter/test.txt'
-    read_file_lines(file_path)
+    parser = argparse.ArgumentParser(description='Youtube to MP3 mass converter')
+    parser.add_argument('-f', '--file', default="links.txt", type=str, help='Path to the input file')
+    parser.add_argument('-o', '--output', default="current_dir", type=str, help='Path to the input file')
+    args = parser.parse_args()
+    read_file_lines(args.file)
--- web.py
+++ web.py
@@ -6,14 +6,12 @@ from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options
 import time
-
+import os
 
 
 def create_driver():
     chrome_options = Options()
     chrome_options.add_argument("--headless")  # Enable headless mode
-
-    # Setup the Chrome WebDriver with headless mode
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
     chrome_options.add_experimental_option("prefs", {
         "download.default_directory": "/Users/ryanroth/Downloads/",  # Set download directory
@@ -29,20 +27,11 @@ def set_input_field(driver, selector, text):
     element.clear()  
     element.send_keys(text)
 
-# Function to click a button
 def click_button(driver, selector):
     element = driver.find_element(By.CSS_SELECTOR, selector)
     element.click()
 
-def click_button_xpath(driver, selector):
-    element = driver.find_element(By.XPATH, selector)
-    element.click()
-
-def send_enter_to_input_field(driver, selector):
-    element = driver.find_element(By.CSS_SELECTOR, selector)
-    element.send_keys(Keys.ENTER)
-
-def click_button_and_go_back(driver, button_selector):
+def click_button_and_close_tab(driver, button_selector):
     original_tab = driver.current_window_handle
     driver.find_element(By.CSS_SELECTOR, button_selector).click()
     time.sleep(1)  
@@ -54,15 +43,23 @@ def click_button_and_go_back(driver, button_selector):
     driver.switch_to.window(original_tab)
 
 def download(driver, link):
-    navigate(driver)
-    set_input_field(driver, "#gatsby-focus-wrapper > div > section.bg-white.p-4.border.border-solid.rounded.border-current.border-solid-clr.container.mx-auto > div > div > div > input", link)
-    click_button_and_go_back(driver, "#gatsby-focus-wrapper > div > section.bg-white.p-4.border.border-solid.rounded.border-current.border-solid-clr.container.mx-auto > div > div > div > button")
-    time.sleep(0.5)
-    click_button_and_go_back(driver,"#gatsby-focus-wrapper > div > section > div > div > div.md\:ml-2 > div > div > table > tbody > tr:nth-child(1) > td.md\:p-\[10px\].p-2.text-center.border.border-gray-300.border-t-0.border-b-1.border-dcdfe4.leading-normal > button")
-    click_button(driver, "#default-modal > div > div > div.p-4.md\:p-5.space-y-4 > div.text-center.mb-6 > a")
-    time.sleep(2)
+    print(f"Attempting to download {link}")
+    try:
+        driver.get('https://y2meta.mobi/en2/youtube-to-mp3/')
+        time.sleep(1)
+        set_input_field(driver, "#gatsby-focus-wrapper > div > section.bg-white.p-4.border.border-solid.rounded.border-current.border-solid-clr.container.mx-auto > div > div > div > input", link)
+        click_button_and_close_tab(driver, "#gatsby-focus-wrapper > div > section.bg-white.p-4.border.border-solid.rounded.border-current.border-solid-clr.container.mx-auto > div > div > div > button")
+        time.sleep(1)
+        click_button_and_close_tab(driver,"#gatsby-focus-wrapper > div > section > div > div > div.md\:ml-2 > div > div > table > tbody > tr:nth-child(1) > td.md\:p-\[10px\].p-2.text-center.border.border-gray-300.border-t-0.border-b-1.border-dcdfe4.leading-normal > button")
+        time.sleep(1)
+        click_button(driver, "#default-modal > div > div > div.p-4.md\:p-5.space-y-4 > div.text-center.mb-6 > a")
+        time.sleep(1)
+        downloads = [element for element in os.listdir(os.getcwd()) if "crdownload" in element]
+        while len(downloads) > 0:
+            time.sleep(0.5)
+            downloads = [element for element in os.listdir(os.getcwd()) if "crdownload" in element] 
+        print(f"Successfully downloaded {link}")
+    except Exception as e:
+        print(e)
+        print(f"Failed to download {link}")
     
-def navigate(driver):
-    driver.get('https://y2meta.mobi/en2/youtube-to-mp3/')
-    time.sleep(1)
-
